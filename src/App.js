211e4ad2eb54c78b60169b6ca37ae6b0b38aae8e
Sebastien Ong Tone <s.ongtone@hotmail.com>
2023-03-11 00:52:17 -0500

    Added loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Space, Layout, Button} from 'antd';
+import {Space, Layout, Button, Spin} from 'antd';
 import {Content, Header} from "antd/es/layout/layout";
 import RowComponent from "./component/rowComponent";
 import {deleteMtgCard, fetchMtgCards, saveMtgCard} from "./api/mtg";
@@ -8,6 +8,7 @@ import Search from "antd/es/input/Search";
 const App = () => {
     const [inputValue, setInputValue] = useState('')
     const [mtgCardList, setMtgCardList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         checkCardList();
@@ -39,27 +40,36 @@ const App = () => {
     }
 
     const onSearch = (value) => {
+        setIsLoading(true);
         saveMtgCard(value).then(() => {
             checkCardList();
             setInputValue('');
+            setIsLoading(false);
         }).catch((err) => {
             console.log(err.message);
+            setIsLoading(false);
         })
     };
 
     const checkCardList = () => {
+        setIsLoading(true);
         fetchMtgCards().then(res => {
             setMtgCardList(createGridList(res));
+            setIsLoading(false);
         }).catch((err) => {
             console.log(err.message);
+            setIsLoading(false);
         })
     }
 
     const onDelete = (value) => {
+        setIsLoading(true);
         deleteMtgCard(value).then(() => {
             checkCardList();
+            setIsLoading(false);
         }).catch((err) => {
             console.log(err.message);
+            setIsLoading(false);
         })
     };
 
@@ -69,28 +79,32 @@ const App = () => {
                 <Layout style={{backgroundColor: '#d0d0e1'}}>
                     <Header style={{backgroundColor: 'transparent', textAlign: 'center'}}>
                         <h1>Magic The Gathering Wish List</h1>
+                    </Header>
+                    <Content style={{borderColor: 'black'}}>
+                        <Button style={{marginTop: '1%', marginLeft: '50%',
+                            transform: 'translate(-50%)',}} onClick={checkCardList}>Refresh Card List</Button>
                         <Search
                             style={{
                                 width: '50%',
                                 marginLeft: '50%',
-                                transform: 'translate(-50%)'
+                                transform: 'translate(-50%)',
+                                marginTop: '1%'
                             }}
                             placeholder="Input link of card"
                             onSearch={onSearch}
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
                             enterButton/>
-                    </Header>
-                    <Content style={{marginTop: '2rem'}}>
-                        <Button onClick={checkCardList}>Refresh Card List</Button>
-
-                        {mtgCardList.map((cardRows) => (
+                        {isLoading ? (<Spin tip="Loading" size="large" style={{marginLeft: '50%',
+                            transform: 'translate(-50%)',
+                            marginTop: '1%'}} />) :
+                            ( <>{mtgCardList.map((cardRows) => (
                             <RowComponent
                                 key={cardRows[0]._id}
                                 cardList={cardRows}
                                 onDelete={onDelete}
                             />
-                        ))}
+                        ))} </>)}
                     </Content>
                 </Layout>
             </Space>
